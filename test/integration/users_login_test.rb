385dceb121160f00669fab170378eb4ec3790aa3
Chadwick Flores <chadwickflores@gmail.com>
2014-11-16 23:54:42 -0600

    add remember me code

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
-  	@user = users(:michael)
+  	@user = users(:chad)
   end
 
   test "login with invalid information" do
@@ -28,6 +28,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a second window.
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
